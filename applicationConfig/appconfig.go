bac51b3ab6b716f389e76961fc24132f8a72a6da
nt_fox <nt_fox@foxmail.com>
2018-09-20 13:55:58 +0800

    增加依赖

--- applicationConfig/appconfig.go
+++ applicationConfig/appconfig.go
@@ -1,5 +1,29 @@
 package applicationConfig
 
+import (
+	"goSvrLib/log"
+	"goSvrLib/userSystem"
+	"io/ioutil"
+
+	jsoniter "github.com/json-iterator/go"
+)
+
+var (
+	Config AppConfig
+)
+
 type AppConfig struct {
-	
+	UserService userSystem.UserServiceParams `json:"userService"`
+}
+
+func InitApplicationConfig(configFilename string) error {
+	bytes, err := ioutil.ReadFile(configFilename)
+	if err != nil {
+		log.Error("config", "ReadFile", err.Error())
+		return err
+	}
+	if err := jsoniter.ConfigCompatibleWithStandardLibrary.Unmarshal(bytes, &Config); err != nil {
+		return err
+	}
+	return nil
 }
